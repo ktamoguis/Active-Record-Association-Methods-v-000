36ce9057691bd3ab3bafad967334d248706d9e31
Karl Amoguis <ktamoguis@gmail.com>
2018-06-07 06:21:28 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -16,6 +16,6 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     binding.pry
-    self.artists.select("name")
+    self.artists
   end
 end
